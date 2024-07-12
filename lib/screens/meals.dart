98999d349397efc97ff92bc390f7d7bd6789cb01
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-12 10:50:48 +0200

    meals data loading added

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -5,20 +5,52 @@ class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
     required this.title,
-    required this.meals,});
+    required this.meals,
+  });
 
   final String title;
   final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Icon(Icons.warning_amber_rounded, size: 64, color: Colors.grey),
+          const SizedBox(height: 16),
+          Text(
+            'Uh oh... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category.',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        // the ListView widget allows to manage a list in an optimal way (showing only the visible items)
+        itemBuilder: (context, index) => Text(
+            // itemBuilder is a function that returns a widget for each item in the list
+            meals[index].title),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: const Center(
-        child: Text('Meals Screen'),
-      ),
+      body: content,
     );
   }
 }
