14d4b90ae27a2774b4bf2773402202f61af0b63a
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-12 11:42:31 +0200

    data passed to the terget screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/category.dart';
 
 import 'package:meals/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals =  dummyMeals
+      .where((meal) => meal.categories.contains(category.id)) 
+      // the body of the function is executed for each element in the list
+      // and must return true if we want to include the element in the new list
+      .toList();
     Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(title: 'Some title...', meals: []),
+        builder: (ctx) => MealsScreen(
+          title: category.title, 
+          meals: filteredMeals,
+        ),
       ),
     );
   }
@@ -38,7 +47,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
                 category: category,
                 onSelectCategory: () {
-                  _selectCategory(context);
+                  _selectCategory(context, category);
                 }),
         ],
         // an alternative is to use the map method
