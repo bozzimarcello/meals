5279cc5d9e04e80d5ca7fdd12421c3f33f6103a3
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-12 11:11:27 +0200

    cross-screen navigation added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 import 'package:meals/data/dummy_data.dart';
@@ -6,26 +7,42 @@ import 'package:meals/data/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(title: 'Some title...', meals: []),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  // good stating point for app with many screens, useful to add a title and a body content
-      appBar: AppBar( // app bar is a header that is displayed at the top of the screen
+    return Scaffold(
+      // good stating point for app with many screens, useful to add a title and a body content
+      appBar: AppBar(
+        // app bar is a header that is displayed at the top of the screen
         title: const Text('Pick your category'),
       ),
       body: GridView(
         padding: const EdgeInsets.all(25), // space around the grid
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount( // layout of the grid
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          // layout of the grid
           crossAxisCount: 2, // number of columns
           childAspectRatio: 3 / 2, // aspect ratio of each grid item
           crossAxisSpacing: 20, // space between columns
           mainAxisSpacing: 20, // space between rows
         ),
         children: [
-          for (final category in availableCategories) // loop through the available categories
-            CategoryGridItem(category: category),
+          for (final category
+              in availableCategories) // loop through the available categories
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context);
+                }),
         ],
-          // an alternative is to use the map method
-          // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+        // an alternative is to use the map method
+        // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
       ),
     );
   }
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -3,20 +3,26 @@ import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
-    super.key, 
-    required this.category});
+    super.key,
+    required this.category,
+    required this.onSelectCategory,
+  });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell( // make the contents tappable with feedback effects vs GestureDetector
-      onTap: () {},
+    return InkWell(
+      // make the contents tappable with feedback effects vs GestureDetector
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
-      child: Container( // allows the config of background color, border, margin, padding, etc
+      child: Container(
+        // allows the config of background color, border, margin, padding, etc
         padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration( // allows the setup of a gradient
+        decoration: BoxDecoration(
+          // allows the setup of a gradient
           gradient: LinearGradient(
             colors: [
               category.color.withOpacity(0.55),
@@ -28,11 +34,12 @@ class CategoryGridItem extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
         ),
         child: Text(
-          category.title, 
-          style: Theme.of(context).textTheme.titleLarge!.copyWith( // see the lesson about themes
-            color: Theme.of(context).colorScheme.onBackground,
-            ),
-          ),
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                // see the lesson about themes
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
