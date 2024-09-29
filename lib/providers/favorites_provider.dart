deb6980fe4daa8123b758c9bc412a038f3a5b28f
Marcello Bozzi <bozzimarcello@gmail.com>
2024-09-29 15:21:35 +0200

    favorites dynamic provider added

---
+++ lib/providers/favorites_provider.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/models/meal.dart';
+
+class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
+  FavoriteMealsNotifier() : super([]);
+
+  void toggleMEalFavoriteStatus(Meal meal) {
+    final mealIsFavorite = state.contains(meal);
+
+    // since in Providers we cannot modify the state directly,
+    // we need to create a new list each time we want to add or remove a meal
+    if (mealIsFavorite) {
+      // create a list without the meal that we want to remove
+      state = state.where((m) => m.id != meal.id).toList();
+    } else {
+      // create a new list with the meal that we want to add
+      // using the spread operator '...' to copy the existing list
+      state = [...state, meal];
+    }
+  }
+}
+
+final favoritesMealsProvider =
+    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+  return FavoriteMealsNotifier();
+});
